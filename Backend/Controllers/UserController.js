a9e39f927939afbf67cee7f0b165e425bca4857a
John Bilunas <johnnyb7184@gmail.com>
2024-06-02 09:31:50 -0400

    Created PUT route for users

--- Backend/Controllers/UserController.js
+++ Backend/Controllers/UserController.js
@@ -34,8 +34,48 @@ userController.getUserInfo = async (req, res, next) => {
 
 }
 
+userController.updateUserInfo = async (req, res, next) => {
 
-//
+    //use to validate properties sent by client
+    const validProperties = {
+        firstname: true, 
+        lastname: true,
+        username: true,
+        email: true,
+        phone: true
+    }
+
+    try{
+        const {_id} = req.user;
+        // get mongoose document for user
+        const userInfo = await User.findById(_id);
+        // error handling if user doesn't exist
+        if(!userInfo){
+            const error = new Error("User doesn't exist");
+            error.statusCode = 400;
+            throw error;
+        }
+        //if key is valid, change the property in the mongoose doc
+        for(const [key, value] of Object.entries(req.body)){
+            if(validProperties[key] === true){
+                userInfo[key] = value;
+            }
+        }
+        //save the doc.
+        await userInfo.save();
+        next()
+    }catch(err){
+        next(err)
+    }
+    
+
+
+
+
+
+
+}
+
 
 userController.getAllUsers = async (req, res, next) => {
 
@@ -45,7 +85,7 @@ userController.getAllUsers = async (req, res, next) => {
         //do not return any sensitive information
         const usersInfo = await User.find().select('-password -patients').exec();
         if(!usersInfo){
-            const error = new Error('Error getting users informatio.');
+            const error = new Error('Error getting users information.');
             error.statusCode = 404;
             throw error;
         }
